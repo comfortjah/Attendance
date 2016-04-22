3502f49167a9771d49706a002d100a0f8193e097
Jake Wert <wertjake@gmail.com>
2016-04-22 14:50:27 -0500

    Added iOS persistence

--- Attendance_iOS/Attendance_iOS/AppDelegate.swift
+++ Attendance_iOS/Attendance_iOS/AppDelegate.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    override init()
+    {
+        Firebase.defaultConfig().persistenceEnabled = true
+    }
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
