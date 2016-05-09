27b1d16fd93adb8b2ab58ad42a90c3e2a878d118
Jake Wert <wertjake@gmail.com>
2016-05-09 14:30:52 -0500

    fixed sign up page

--- Attendance_Web/public/js/signup.js
+++ Attendance_Web/public/js/signup.js
@@ -6,7 +6,51 @@ function($scope, $firebaseAuth)
   var ref = new Firebase("https://attendance-cuwcs.firebaseio.com/");
   var auth = $firebaseAuth(ref);
 
+  $scope.login = function()
+  {
+    ref.authWithPassword(
+      {
+        email    : $scope.email,
+        password : $scope.password
+      },
+      function(error, authData)
+      {
+        if (error)
+        {
+          console.log("Login Failed!", error);
+        }
+        else
+        {
+          console.log("Authenticated successfully with payload:", authData);
 
+          $scope.setInstructor(authData.uid);
+        }
+      });
+  };
+
+  $scope.setInstructor = function(uid)
+  {
+    var instructorRef = ref.child("Instructors").child(uid);
+
+    instructorRef.set(
+    {
+      firstName: $scope.firstName,
+      lastName: $scope.lastName
+    },
+    function(error)
+    {
+      if (error)
+      {
+        console.log("Unable to create instructor object. Please have the admin remove your account before trying again.");
+      }
+      else
+      {
+        console.log("Successfully created instructor object.");
+
+        window.location.href = 'dashboard.html';
+      }
+    });
+  }
 
   $scope.signup = function()
   {
@@ -25,14 +69,7 @@ function($scope, $firebaseAuth)
       {
         console.log("Successfully created user account with uid:", userData.uid);
 
-        var instructorRef = ref.child("Instructors").child(userData.uid);
-
-
-        instructorRef.set(
-        {
-          firstName: $scope.firstName,
-          lastName: $scope.lastName,
-        });
+        $scope.login();
       }
     });
   }
